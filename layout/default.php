2664fbbcfc3634531d8219f4e52587b6833dab95
home <home@home-ZBOX.(none)>
2013-05-28 15:48:48 +0300

    Framework null

--- layout/default.php
+++ layout/default.php
@@ -1,13 +1,9 @@
-
 <!DOCTYPE html>
 <html>
     <head>
         <title>Framework v0.0</title>
     </head>
     <body>
-        <!-- <h1>Huy Masika - prekrasen!!!</h1> -->
         <?php echo $content; ?>
-        
-        
     </body>
-</html>    
+</html>
